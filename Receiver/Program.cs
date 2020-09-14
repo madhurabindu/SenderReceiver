82b1453eeb0505d1d31cf09e819d57436cb3a061
Madhura Bindu <madhura.bindu@xymob.in>
2020-09-14 19:13:27 +0530

    sample

---
+++ Receiver/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press enter to start receiving data");
+            Console.ReadLine();
+            Console.WriteLine("Waiting for data .... ");
+            for (int i = 0; i < 10; i++)
+            {
+                string input  = Console.ReadLine();
+                Console.WriteLine("Receiver: " + input);
+            }
+        }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SenderReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press enter to start sending data");
+            Console.ReadLine();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Sender Hello" + i.ToString());
+            }
+        }
+    }
+}
